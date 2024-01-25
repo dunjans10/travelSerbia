e258340af019ae9537e53b78342bd8911d15ca26
Dunja <dunjabeki@gmail.com>
2024-01-25 15:32:55 +0100

    Adding scroll bar, scroll up and active link

--- script.js
+++ script.js
@@ -38,3 +38,41 @@ const blurHeader = () => {
 }
 
 window.addEventListener('scroll', blurHeader)
+
+
+//Show scroll up
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >=350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+//Scroll sections - Active link
+
+const sections = document.querySelectorAll('section[id');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id'),
+    sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      sectionClass.classList.add('active-link')
+    }else{
+      sectionClass.classList.remove('active-link')
+    }
+
+
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
